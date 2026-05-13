d69dc5232aa0543cca91006dcf86644e09034d0b
justappgrabbin <bookingalexismcmillion@gmail.com>
2026-05-13 00:56:47 -0700

    Load morph state accessor before visualizer render

--- client/src/components/NeuralMeshVisualizer.tsx
+++ client/src/components/NeuralMeshVisualizer.tsx
@@ -6,6 +6,7 @@
  */
 
 import React, { useEffect, useRef, useState } from 'react'
+import '@/lib/orchestratorMorphStatePatch'
 import { ResonanceOrchestrator, NodeState, Address } from '@/lib/orchestrator'
 
 interface NeuralMeshVisualizerProps {
